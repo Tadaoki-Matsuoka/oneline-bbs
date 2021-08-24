fe3853419d54487df466bc91ed6d74c41d814bf0
Tadaoki Matsuoka <matsuoka.tadaoki@no1s.biz>
2021-08-24 13:07:11 +0900

    first commit

---
+++ class/Validation.class.php
@@ -0,0 +1,10 @@
+<?php
+class Validation {
+    static function isRequired($param) {
+        if(empty($param)){
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
